0625812f45789f364bb220a7802f2d38eb1280e5
ousjaguraga <ousjaguraga19@gmail.com>
2021-08-19 23:41:29 +0100

    B added to my name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Ousman and i am an Ovenger
+          My name is Ousman B and i am an Ovenger.
         </p>
         
       </header>
